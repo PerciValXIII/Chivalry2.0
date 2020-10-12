fbce11904bbe86e5d6c24c2dbb42302cbc7b46bc
Swattik Maiti <50179165+SwattikMaiti@users.noreply.github.com>
2020-10-12 15:08:38 +0530

    Add files via upload

---
+++ build/precache-manifest.3a7940583d59f232c2c3b775e2b89a93.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd189a7a3ed2b9afee4fa8b04711089f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10cb90b716df2d3da085",
+    "url": "/static/css/main.c4d706d5.chunk.css"
+  },
+  {
+    "revision": "d54482d33d7d14f7d8ef",
+    "url": "/static/js/2.73aff2ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.73aff2ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10cb90b716df2d3da085",
+    "url": "/static/js/main.1d58279d.chunk.js"
+  },
+  {
+    "revision": "8f236be898f101036547",
+    "url": "/static/js/runtime-main.433a3f71.js"
+  },
+  {
+    "revision": "92c84e1de1513c18c5026987b2567539",
+    "url": "/static/media/image2.92c84e1d.png"
+  },
+  {
+    "revision": "ded1a9c6781f8a610cb3ba6902c1b398",
+    "url": "/static/media/image3.ded1a9c6.png"
+  },
+  {
+    "revision": "7e36cd7ece6ca93ce4d9d41235b72360",
+    "url": "/static/media/svg-3.7e36cd7e.svg"
+  },
+  {
+    "revision": "61e69671f5a2beb23812ff6f5c99ea31",
+    "url": "/static/media/svg-4.61e69671.svg"
+  },
+  {
+    "revision": "1f7f8501643547a750b82343c70a0a22",
+    "url": "/static/media/svg-6.1f7f8501.svg"
+  },
+  {
+    "revision": "f7fef37fc94c5a601c158998c526fec8",
+    "url": "/static/media/video.f7fef37f.mp4"
+  }
+]);
